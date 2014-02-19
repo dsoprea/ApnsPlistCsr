078e0944570e5a7aa24a09dfb8a4b1aa8db1d7e4
Dustin Oprea <myselfasunder@gmail.com>
2014-02-19 18:58:40 -0500

    Encoded CSR-plists are now accepted.

--- apns_csr/builder.py
+++ apns_csr/builder.py
@@ -1,20 +1,24 @@
+import urllib2
 import ssl
 import M2Crypto
-import requests
-import urllib2
+import OpenSSL
 
 from plistlib import writePlistToString
 from base64 import b64encode
 from hashlib import sha1
+from collections import OrderedDict
+
+IA_CERT_URL = 'https://developer.apple.com/certificationauthority/AppleWWDRCA.cer'
+CA_CERT_URL = 'http://www.apple.com/appleca/AppleIncRootCertificate.cer'
 
 def _get_remote_cert(url):
-    r = requests.get(url)
-    data = r.raw.read()
+# TODO(dustin): There's an encoding problem with requests.get(url).text, and partial data when we do requests.get(url).raw.read() .
+# TODO(dustin): Cache/store this locally, or we'll be out of luck if their systems are unavailable.
+    data = urllib2.urlopen(url).read()
+    if not data:
+        raise ValueError("Could not read remote certificate: %s" % (url))
 
-# TODO(dustin): urllib2 was in the original design, and it produces a result that's twice as long. We don't know why, or which version of correct.
-# data = urllib2.urlopen(url).read()
-
-    return _cer_der_to_pem(data)
+    return _cer_der_to_pem(data).rstrip() + "\n"
 
 def _verify_csr(csr_text):
     M2Crypto.X509.load_request_string(csr_text)
@@ -22,49 +26,80 @@ def _verify_csr(csr_text):
 def _verify_private_key(private_key_text):
     M2Crypto.RSA.load_key_string(private_key_text)
 
-def _verify_mdm_vendor_certificate(mdm_vendor_certificate_der):
-    M2Crypto.X509.load_cert_der_string(mdm_vendor_certificate_der)
+def _verify_and_read_mdm_vendor_certificate(mdm_vendor_p12_certificate_der, passphrase):
+    mdm_vendor_certificate = OpenSSL.crypto.load_pkcs12(mdm_vendor_p12_certificate_der, passphrase)
+
+    mdm_vendor_pk_internal = mdm_vendor_certificate.get_privatekey()
+    mdm_vendor_certificate_internal = mdm_vendor_certificate.get_certificate()
+
+    mdm_vendor_pk_pem = OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, mdm_vendor_pk_internal)
+    mdm_vendor_certificate_pem = OpenSSL.crypto.dump_certificate(OpenSSL.crypto.FILETYPE_PEM, mdm_vendor_certificate_internal)
+
+    return { 'private_key_pem': mdm_vendor_pk_pem, 
+             'certificate_pem': mdm_vendor_certificate_pem }
 
 def _get_der_from_pem_csr(csr_text):
     r = M2Crypto.X509.load_request_string(csr_text)
     return r.as_der()
 
-def _sign_csr(csr_der, private_key_text):
-    private_key = M2Crypto.RSA.load_key_string(private_key_text)
+def _sign_csr(csr_der, mdm_vendor_private_key_text):
+    mdm_vendor_private_key = M2Crypto.RSA.load_key_string(mdm_vendor_private_key_text)
+    hashed = sha1(csr_der).digest()
+    signature = mdm_vendor_private_key.sign(hashed)
 
-# TODO(dustin): Is DER alirght? Is SHA1 what we're supposed to be using?
-# TODO(dustin): Verify that this returns pure binary.
-    signature = private_key.sign(sha1(csr_der).digest())
     return b64encode(signature)
 
 def _cer_der_to_pem(cer_certificate_der):
-# TODO(dustin): Verify that this works.
     return ssl.DER_cert_to_PEM_cert(cer_certificate_der)
 
-def mdm_vendor_sign(csr_text, private_key_text, mdm_vendor_certificate_der):
+def _chunks(l, n=64):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n] + "\n"
+
+def mdm_vendor_sign(csr_text, 
+                    private_key_text, 
+                    mdm_vendor_certificate_der, 
+                    mdm_vendor_certificate_passphrase, 
+                    encode_xml=True):
     _verify_csr(csr_text)
     _verify_private_key(private_key_text)
-    _verify_mdm_vendor_certificate(mdm_vendor_certificate_der)
+    
+    mdm_vendor_cert_info = \
+        _verify_and_read_mdm_vendor_certificate(
+            mdm_vendor_certificate_der, 
+            mdm_vendor_certificate_passphrase)
 
     csr_der = _get_der_from_pem_csr(csr_text)
     csr_der_b64 = b64encode(csr_der)
 
-    csr_signature = _sign_csr(csr_der, private_key_text)
+    csr_signature = _sign_csr(csr_der, mdm_vendor_cert_info['private_key_pem'])
 
-    ia_pem = _get_remote_cert('https://developer.apple.com/certificationauthority/AppleWWDRCA.cer')
-    root_pem = _get_remote_cert('http://www.apple.com/appleca/AppleIncRootCertificate.cer')
+    ia_pem = _get_remote_cert(IA_CERT_URL)
+    ca_pem = _get_remote_cert(CA_CERT_URL)
     mdm_vendor_certificate_pem = _cer_der_to_pem(mdm_vendor_certificate_der)
 
+    _request_csr = ''.join(_chunks(csr_der_b64))
+    _cert_chain = (mdm_vendor_cert_info['certificate_pem'] + ia_pem + ca_pem)
+    _cert_sig = ''.join(_chunks(csr_signature))
+
     plist_dict = dict(
-        PushCertRequestCSR=csr_der_b64,
-        PushCertCertificateChain=mdm_vendor_certificate_pem + ia_pem + root_pem,
-        PushCertSignature=csr_signature
-    )
+        PushCertRequestCSR=_request_csr.rstrip(),
+        PushCertCertificateChain=_cert_chain.rstrip(),
+        PushCertSignature=_cert_sig.rstrip())
 
     plist_xml = writePlistToString(plist_dict)
-    return b64encode(plist_xml)
 
-def mdm_vendor_sign_with_files(private_key_filepath, csr_filepath, mdm_vendor_certificate_filepath):
+    if encode_xml is False:
+        return plist_xml
+
+    encoded_plist = b64encode(plist_xml)
+    return ''.join(_chunks(encoded_plist)).rstrip() + "\n"
+
+def mdm_vendor_sign_with_files(private_key_filepath, 
+                               csr_filepath, 
+                               mdm_vendor_certificate_filepath, 
+                               mdm_vendor_certificate_passphrase, 
+                               *args, **kwargs):
     with open(csr_filepath) as f:
         csr_text = f.read()
 
@@ -74,4 +109,8 @@ def mdm_vendor_sign_with_files(private_key_filepath, csr_filepath, mdm_vendor_ce
     with open(mdm_vendor_certificate_filepath) as f:
         mdm_vendor_certificate_der = f.read()
 
-    return mdm_vendor_sign(csr_text, private_key_text, mdm_vendor_certificate_der)
+    return mdm_vendor_sign(csr_text, 
+                           private_key_text, 
+                           mdm_vendor_certificate_der, 
+                           mdm_vendor_certificate_passphrase, 
+                           *args, **kwargs)
--- scripts/csr_to_apns_csr.py
+++ scripts/csr_to_apns_csr.py
@@ -7,13 +7,31 @@ import argparse
 
 from apns_csr import mdm_vendor_sign_with_files
 
-parser = argparse.ArgumentParser(description="Produce an APNS Plist (encoded) from a standard CSR.")
-parser.add_argument('key', help='Private key (PEM)')
-parser.add_argument('csr', help='Certificate signing request (PEM)')
-parser.add_argument('mdm', help='MDM vendor certificate (DER)')
+parser = argparse.ArgumentParser(
+            description="Produce an APNS Plist (encoded) from a standard CSR.")
+
+parser.add_argument('key', 
+                    help='Private key (PEM)')
+
+parser.add_argument('csr', 
+                    help='Certificate signing request (PEM)')
+
+parser.add_argument('mdm', 
+                    help='MDM vendor certificate (DER)')
+
+parser.add_argument('mdm_passphrase', 
+                    help='Passphrase for MDM vendor certificate')
+
+parser.add_argument('-x', '--xml', 
+                    help='Show raw XML', action='store_true')
 
 args = parser.parse_args()
 
-encoded_plist = mdm_vendor_sign_with_files(args.key, args.csr, args.mdm)
+encoded_plist = mdm_vendor_sign_with_files(
+                    args.key, 
+                    args.csr, 
+                    args.mdm, 
+                    args.mdm_passphrase, 
+                    encode_xml=not args.xml)
 
-print(encoded_plist)
+sys.stdout.write(encoded_plist)
